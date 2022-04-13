452ff0be4cfc3fcc48726eaacc21fa936c5cb304
sivabhargavi <sivabhargavi.vsoftforyou@gmail.com>
2022-04-13 20:20:03 +0530

    Create controller

---
+++ app/config/routes.php
@@ -0,0 +1,5 @@
+<?php
+// config/routes.php
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) { };
---
+++ app/src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+   function test()
+   {
+       echo "Test";
+   }
+    
+}
