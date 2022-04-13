f7e5ee6ef8874186edb8850b1a68a4f08d325a03
shalinilli <shalini@novastrid.com>
2022-04-14 01:23:35 +0530

    Competition list UI integration

--- app/assets/js/components/pages/Competitions.js
+++ app/assets/js/components/pages/Competitions.js
@@ -5,9 +5,45 @@ class Competitions extends Component {
 
     render() {
         return (
-            <div >
-                <div aria-checked="false" className="bg-gray-200 relative inline-block flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline" role="checkbox" tabIndex="0">
-                test
+            <div className="bg-bodybg min-h-screen p-4">
+
+                <div className="bg-white pt-8 shadow-whitebox rounded-whitebox">
+                    <h1 className="uppercase -m-8 mb-0 p-6 px-8 rounded-t-whitebox font-bold text-center border-b border-b-eee text-2xl">Competitions List</h1>
+                        <div className="scroller scroll-team p-8 pt-16 inline-block w-full">
+                            {/* Competitions list starts */}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 min-h-full">
+                                    <a className="bg-white pt-8 p-8 pb-4 drop-shadow-whitebox rounded-whitebox text-center">
+                                        <span className="max-w-7rm flex items-center justify-center m-auto text-primary mb-8">
+                                            <img src="https://crests.football-data.org/BL1.png" />
+                                        </span>
+                                        <h6 className="text-textcolor text-sm border-t font-bold border-slate-100 pt-4 -mx-8">Bundesliga</h6>
+                                    </a>
+
+                                    <a className="bg-white pt-8 p-8 pb-4 drop-shadow-whitebox rounded-whitebox text-center">
+                                        <span className="max-w-7rm flex items-center justify-center m-auto text-primary mb-8">
+                                            <img src="https://crests.football-data.org/PD.png" />
+                                        </span>
+                                        <h6 className="text-textcolor text-sm border-t font-bold border-slate-100 pt-4 -mx-8">Primera Division</h6>
+                                    </a>
+
+                                    <a className="bg-white pt-8 p-8 pb-4 drop-shadow-whitebox rounded-whitebox text-center">
+                                        <span className="max-w-7rm flex items-center justify-center m-auto text-primary mb-8">
+                                            <img src="https://crests.football-data.org/FL1.png" />
+                                        </span>
+                                        <h6 className="text-textcolor text-sm border-t font-bold border-slate-100 pt-4 -mx-8">Ligue 1</h6>
+                                    </a>
+
+                                    <a className="bg-white pt-8 p-8 pb-4 drop-shadow-whitebox rounded-whitebox text-center">
+                                        <span className="max-w-7rm flex items-center justify-center m-auto text-primary mb-8">
+                                            <img src="https://crests.football-data.org/PL.png" />
+                                        </span>
+                                        <h6 className="text-textcolor text-sm border-t font-bold border-slate-100 pt-4 -mx-8">Premier League</h6>
+                                    </a>
+                            </div>
+
+                            {/* Competitions list ends */}
+                        </div>
+
                 </div>
             </div>
         )
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,6 +5,66 @@ module.exports = {
     "./assets/**/*.{vue,js,ts,jsx,tsx}",
     "./templates/**/*.{html,twig}"
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        'bodyfont': 'Mulish',
+      },
+      colors: {
+        'textcolor': '#585757',
+        'primary': '#00C5FF',
+        'menu': '#7C8AB5',
+        'secondary': '#830d1a',
+        'paracolor': '#929191',
+      },
+      backgroundColor: {
+        'bodybg': '#E5E5E5',
+        'flagbg': '#FFF1F5',
+        'primary': '#00C5FF',
+        'secondary': '#830d1a',
+        'headingbg': '#EDF2F7',
+        'modalhead': '#F0F1F2',
+        'iconbg': 'rgba(0,0,0,.1)',
+      },
+      spacing: {
+        '5px': '500px',
+      },
+      fontSize: {
+        'fs12': '12px',
+        'fs150': '150px',
+      },
+      boxShadow: {
+        'whitebox': '0px 20px 40px 15px rgba(226, 226, 226, 0.2)',
+      },
+      borderWidth: {
+        '7': '7px',
+      },
+      borderColor: {
+        'secondary': '#830d1a',
+      },
+      borderRadius: {
+        'whitebox': '20px',
+        'flagradius': '10px',
+        'radius5': '5px',
+      },
+      dropShadow: {
+        'whitebox': '0px 30px 60px rgba(50, 50, 71, 0.06)',
+      },
+      blur: {
+        sm: '2px',
+      },
+      maxWidth: {
+        '7rm': '7rem',
+        '10rm': '10rem',
+      },
+      minWidth: {
+        '3xl': '48rem',
+      },
+      maxHeight: {
+        '40vh': '40vh',
+        '72vh': '72vh',
+      }
+    },
+  },
   plugins: [],
 }
