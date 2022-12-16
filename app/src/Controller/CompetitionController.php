5e5c914237838516a4bce69c9d504f7b24201d37
kokilavani.p <kokilavani.p@arkinfotec.com>
2022-12-16 19:58:09 +0530

    Chnages Update

--- app/src/Controller/CompetitionController.php
+++ app/src/Controller/CompetitionController.php
@@ -11,45 +11,54 @@ use App\Service\CompetitionManagementService;
 class CompetitionController extends AbstractController
 {
 	public function __construct(
-		private readonly CompetitionManagementService $CompetitionManagementService,
-	){}
+		private readonly CompetitionManagementService $CompetitionManagementService
+	) {
+	}
 
-	#[Route('/competitions', name: 'competition')]
-	public function competition(): Response
-    {
-		$filepath    = $this->getParameter('kernel.project_dir') . '/data/competitions/competitions.json';
-		$competitions = $this->CompetitionManagementService->getCompetitions($filepath);
-	
+	#[Route('/competitions', name: 'competitions')]
+	public function competition(Request $request): Response
+	{
+		$path = $request->getPathInfo();
+		$rootpath =  $this->filepath();
+		$competitions = $this->CompetitionManagementService->getCompetitions($rootpath, "/competitions" . $path);
+
 		return $this->render('Competition/competition.html.twig', ['competitions' => $competitions['competitions']]);
-    }
+	}
 
-	#[Route('/competitions/team/{id}', name: 'team')]
+	#[Route('/competitions/teams/{id}', name: 'team')]
 	public function team(Request $request): Response
 	{
-		$id = $request->get('id');
-		$filepath = $this->getParameter('kernel.project_dir') . '/data/competitions/teams/' . $id . '.json';
-		$teams = $this->CompetitionManagementService->getCompetitions($filepath);
-		
+		$path = $request->getPathInfo();
+		$rootpath =  $this->filepath();
+		$teams = $this->CompetitionManagementService->getCompetitions($rootpath, $path);
+
 		return $this->render('Competition/team.html.twig', ['teams' => $teams['teams']]);
 	}
 
-	#[Route('/competitions/match/{id}', name: 'match')]
+	#[Route('/competitions/matches/{id}', name: 'match')]
 	public function match(Request $request): Response
 	{
-		$id = $request->get('id');
-		$filepath = $this->getParameter('kernel.project_dir') . '/data/competitions/matches/' . $id . '.json';
-		$matches = $this->CompetitionManagementService->getCompetitions($filepath);
+		$path = $request->getPathInfo();
+		$rootpath =  $this->filepath();
+		$matches = $this->CompetitionManagementService->getCompetitions($rootpath, $path);
 
 		return $this->render('Competition/match.html.twig', ['matches' => $matches['matches']]);
 	}
 
-	#[Route('/team/players/{id}', name: 'player')]
+	#[Route('/teams/{id}', name: 'player')]
 	public function player(Request $request): Response
 	{
-		$id = $request->get('id');
-		$filepath = $this->getParameter('kernel.project_dir') . '/data/teams/' . $id . '.json';
-		$teamplayers = $this->CompetitionManagementService->getCompetitions($filepath);
-		
+		$path = $request->getPathInfo();
+		$rootpath =  $this->filepath();
+		$teamplayers = $this->CompetitionManagementService->getCompetitions($rootpath, $path);
+
 		return $this->render('Competition/player.html.twig', ['players' => $teamplayers['squad']]);
+	}
+
+	public function filepath()
+	{
+		$filepath = $this->getParameter('kernel.project_dir');
+
+		return $filepath;
 	}
 }
